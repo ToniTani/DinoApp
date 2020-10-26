2a91964ea8018e6e21a42c8f6c181603b79f4e1d
ToniTani <toni.tani@student.saimia.fi>
2020-10-26 04:13:54 +0200

    first version of timer is now working.

--- app/src/main/java/com/example/dinoapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/dinoapp/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,13 @@
 package com.example.dinoapp.ui.notifications;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,20 +20,110 @@ import com.example.dinoapp.R;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    TextView timerTextView;
+    SeekBar timerSeekBar;
+    CountDownTimer countDownTimer;
+    boolean timerIsActive = false;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        timerSeekBar = view.findViewById(R.id.timerSeekBar);
+        timerTextView = view.findViewById(R.id.timerTextView);
+
+
+
+
+        final ImageButton playImgButton = view.findViewById(R.id.playImgButton);
+        playImgButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                timerSeekBar.setEnabled(false);
+                countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        updateTimer((int) millisUntilFinished / 1000);
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(getActivity().getApplicationContext(),R.raw.tollbell);
+                        mediaPlayer.start();
+                        resetTimer();
+                    }
+
+                }.start();
+                timerIsActive = true;
+                playImgButton.setVisibility(View.INVISIBLE);
+
             }
         });
-        return root;
+
+        ImageButton pauseImgButton = view.findViewById(R.id.pauseImgButton);
+        pauseImgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+                timerIsActive = false;
+                playImgButton.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        ImageButton stopImgButton = view.findViewById(R.id.stopImgButton);
+        stopImgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTimer(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+        return view;
+    }
+    public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60; // number of seconds divided by 60, converts int
+        int seconds = secondsLeft - (minutes * 60); // to find seconds
+
+        String secondString = Integer.toString(seconds);
+
+                if (seconds <= 9) {
+                secondString = "0" + secondString; }
+                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+    public void resetTimer() {
+        timerTextView.setText("00:30");
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setEnabled(true);
+        timerIsActive = false;
+
     }
 }
